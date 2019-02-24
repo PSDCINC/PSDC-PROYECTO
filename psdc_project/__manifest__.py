4cc1851c4b057131cf98e4613fa6fcdc98ffa6aa
PSDCINC <47335169+PSDCINC@users.noreply.github.com>
2019-02-23 22:32:30 -0500

    Versión inicial de Modulo de Proyecto vs11

---
+++ psdc_project/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "PSDC - Ajustes para Proyectos y Contactos",
+    "summary": "Ajustes al módulo de proyectos y contactos",
+    "description": """
+        PSDC - proyectos y contactos, contiene información para:
+            - Definir contactos como residentes
+            - Agregar residentes en los proyectos
+            - Agregar adendas
+            - Agregar fianzas
+            - Agregar pólizas
+    """,
+    "author": "PSDC Innova",
+    "website": "https://psdc.com.pa/",
+    "category": "Projects",
+    "version": "1.1.0",
+    "depends": ['base', 'contacts', 'project'],
+    "data": [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/res_partner_views.xml',
+        'views/planning_views.xml',
+        'views/project_views.xml',
+        'views/addendum_views.xml',
+        'views/endorsement_views.xml',
+        'views/bail_views.xml',
+        'views/policy_views.xml',
+        'views/task_history_views.xml',
+        'views/subproject_views.xml',
+        'data/addendum_descriptions.xml',
+        'data/endorsement_reasons.xml',
+        'data/policy_types.xml',
+    ],
+    "demo": []
+}
